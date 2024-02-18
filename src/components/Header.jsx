224e5e59007dd8acdef654b71cfe9383a69d36ce
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-18 15:32:05 +0300

    Header component css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import "../styles/header.css";
+import { HiShoppingCart, HiSearch } from "react-icons/hi";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+      <Navbar />
+    </header>
+  )
+};
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="header--nav">
+        <div className="nav--logo">
+          <a href="#">
+            <h2>PetCraze</h2>
+          </a>
+        </div>
+        <div className="nav--divider"></div>
+        <ul className="nav--list">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#category">Category</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#footer">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="nav--icons">
+        <HiShoppingCart />
+        <HiSearch />
+      </div>
+    </>
   )
 }
 

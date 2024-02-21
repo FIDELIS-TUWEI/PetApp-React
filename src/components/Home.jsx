77ca148a11018fa23859fba451ee7fe7662a3cd5
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-21 05:39:06 +0300

    Home page component UI design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,28 @@
+import "../styles/home.css";
+import { HiOutlinePlay } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home" id="home">
+      <div className="home--content">
+        <h2 className="home--title">
+          Bringing <span>Pets</span>
+          <br />& People Closer
+        </h2>
+
+        <p className="home--description">
+          Unleash the excitement of the pet world - where passion and companionship come together in perfect harmony.
+        </p>
+
+        <div className="home--buttons">
+          <button className="btn--1">Adopt Now</button>
+          <button className="btn--2">
+            <HiOutlinePlay className="btn--icon" /> Watch Video
+          </button>
+        </div>
+      </div>
+      <div className="home-image"></div>
+    </section>
   )
 }
 

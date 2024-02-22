8d67430e7131e34193ccac9fd4a1fc2879ae2e52
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-22 05:22:24 +0300

    Pet data added

---
+++ src/Data/petData.js
@@ -0,0 +1,35 @@
+import dogImage from "../assets/dog.jpg";
+import catImage from "../assets/cat.jpg";
+import parrotImage from "../assets/parrot.jpg";
+import rabbitImage from "../assets/rabbit.jpg";
+
+export const petData = [
+    {
+        id: 1,
+        title: "Dog",
+        imgUrl: dogImage,
+        price: 900,
+        rating: 4.9,
+    },
+    {
+        id: 2,
+        title: "Cat",
+        imgUrl: catImage,
+        price: 800,
+        rating: 4.8,
+    },
+    {
+        id: 3,
+        title: "Parrot",
+        imgUrl: parrotImage,
+        price: 700,
+        rating: 4.5,
+    },
+    {
+        id: 4,
+        title: "Rabbit",
+        imgUrl: rabbitImage,
+        price: 300,
+        rating: 4.5,
+    },
+]

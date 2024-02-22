7f07232b46443279b97ed4652ed534ff2aae18a2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-22 05:30:47 +0300

    UI container

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,7 +2,12 @@ import "../styles/category.css";
 
 const Category = () => {
   return (
-    <section className="category" id="category">Category</section>
+    <section className="category" id="category">
+        <p className="section--slogan">Customer Favorites</p>
+        <h2 className="section--heading">Popular Categories</h2>
+
+        <div className="category--container"></div>
+    </section>
   )
 }
 

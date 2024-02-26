60ca78e108c9397f8a1bd11c602013f871fa5915
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-26 13:57:10 +0300

    Petlist UI component

--- src/components/PetList.jsx
+++ src/components/PetList.jsx
@@ -1,8 +1,28 @@
 import "../styles/petList.css";
+import { PetData } from "../Data/PetData";
 
 const PetList = () => {
   return (
-    <section className="pets">PetList</section>
+    <section className="pets">
+      <p className="section--slogan">Recommendation</p>
+      <h2 className="section--heading">Best Choice</h2>  
+
+      <div className="pets--container">
+        {PetData.map((pet) => (
+          <div className="pet--box" key={pet.id}>
+            <img className="pet--image" src={pet.imgUrl} alt="" />
+
+            <h1 className="pet--title">
+              {pet.title}
+            </h1>
+
+            <h4 className="pet--price">
+              ${pet.price}
+            </h4>
+          </div>
+        ))}
+      </div>  
+    </section>
   )
 }
 

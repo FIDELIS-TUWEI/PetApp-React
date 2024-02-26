8629a59c2b4e3d6d7004558f1e347174477518b3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-26 05:39:16 +0300

    Banner component UI design

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,22 @@
 import "../styles/banner.css";
+import groupImage from "../assets/pet-group.png"
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <section className="banner--section">
+      <div className="banner">
+        <div className="banner--content">
+          <h2>Stay & Save</h2>
+          <h2>Get your awesome discount</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div className="banner--image">
+          <img src={groupImage} alt="pet-group" />
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default Banner
+export default Banner;

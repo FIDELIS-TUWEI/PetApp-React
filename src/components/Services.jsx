11a71637b074e3c4631dc5f4e9db47a004e419b1
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-27 14:02:34 +0300

    services UI design

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,59 @@
+import { HiGlobe } from "react-icons/hi";
 import "../styles/services.css";
+
+const services = [
+    {
+        id: 1,
+        title: 'best grooming',
+        icon: <HiGlobe />
+    },
+    {
+        id: 2,
+        title: 'dog training',
+        icon: <HiGlobe />
+    },
+    {
+        id: 3,
+        title: 'special diet',
+        icon: <HiGlobe />
+    },
+    {
+        id: 4,
+        title: 'vet services',
+        icon: <HiGlobe />
+    },
+]
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="services"id="services">
+        <div className="service--content">
+            <p className="section--slogan">
+                services
+            </p>
+            <h2 className="section--heading">
+                Pet Care With Love
+            </h2>
+
+            <div className="services--des">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae tempora ea perferendis unde, repudiandae corrupti atque dolorem! Magnam, et? Ipsum quod saepe eius hic asperiores nobis repellat dolorum, repellendus similique?
+            </div>
+            <button className="btn--1">Explore</button>
+        </div>
+        
+        <div className="services--container">
+            {services.map((service) => (
+                <div className="service--box" key={service.id}>
+                    <span className="service--icon">
+                        {service.icon}
+                    </span>
+                    <h3 className="service--title">
+                        {service.title}
+                    </h3>
+                </div>
+            ))}
+        </div>
+    </section>
   )
 }
 

9d32452fa35802df3fd20ee7524f7f534c08a69f
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-29 14:10:24 +0300

    Footer UI design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,43 @@
 import "../styles/footer.css";
 
+import { BsFacebook, BsInstagram, BsTwitterX, BsYoutube } from "react-icons/bs"
+
 const Footer = () => {
   return (
-    <section>Footer</section>
+    <section className="footer" id="footer">
+      <div className="footer--news--feed">
+        <div className="news--feed--text">
+          <h3>Subscribe to our News Letter!</h3>
+        </div>
+        <div className="news--feed--input">
+          <input type="text" placeholder="Enter your Email" />
+          <button>Subscribe</button>
+        </div>
+      </div>
+
+      <div className="footer--menu">
+        <ul className="footer--menu--list">
+          <li><a href="#">Home</a></li>
+          <li><a href="">Category</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+
+        <div className="footer--menu--icons">
+          <BsFacebook />
+          <BsInstagram />
+          <BsTwitterX />
+          <BsYoutube />
+        </div>
+      </div>
+
+      <div className="footer--breaker"></div>
+      <div className="footer--credits">
+        <p>
+          &copy; {new Date().getFullYear()} - PetCraze All rights reserved.
+        </p>
+      </div>
+    </section>
   )
 }
 

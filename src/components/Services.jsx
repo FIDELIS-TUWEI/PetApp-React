87480db557cd8fd77a320a3598e3c488ef598287
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-03 11:51:53 +0300

    framer-motion configuration

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,6 @@
 import { HiGlobe } from "react-icons/hi";
 import "../styles/services.css";
+import { motion } from "framer-motion";
 
 const services = [
     {
@@ -27,7 +28,12 @@ const services = [
 const Services = () => {
   return (
     <section className="services"id="services">
-        <div className="service--content">
+        <motion.div 
+            className="service--content"
+            initial={{ opacity: 0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 2 }}
+        >
             <p className="section--slogan">
                 services
             </p>
@@ -39,18 +45,24 @@ const Services = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae tempora ea perferendis unde, repudiandae corrupti atque dolorem! Magnam, et? Ipsum quod saepe eius hic asperiores nobis repellat dolorum, repellendus similique?
             </div>
             <button className="btn--1">Explore</button>
-        </div>
+        </motion.div>
         
         <div className="services--container">
             {services.map((service) => (
-                <div className="service--box" key={service.id}>
+                <motion.div 
+                    className="service--box" 
+                    key={service.id}
+                    initial={{ opacity: 0, scale: 0 }}
+                    whileInView={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 2 }}
+                >
                     <span className="service--icon">
                         {service.icon}
                     </span>
                     <h3 className="service--title">
                         {service.title}
                     </h3>
-                </div>
+                </motion.div>
             ))}
         </div>
     </section>

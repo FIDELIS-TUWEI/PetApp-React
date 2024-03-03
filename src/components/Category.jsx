3328789e764563a14a37def4b504a786e695c366
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-03 11:51:38 +0300

    framer-motion configuration

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,5 +1,6 @@
 import "../styles/category.css";
 import { PetData } from "../Data/PetData";
+import { motion } from "framer-motion";
 
 const Category = () => {
   return (
@@ -9,10 +10,16 @@ const Category = () => {
 
         <div className="category--container">
           {PetData.map((data) => (
-            <div className="category--box" key={data.id}>
+            <motion.div 
+              className="category--box" 
+              key={data.id}
+              initial={{ opacity: 0, scale: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 2 }}
+            >
               <img className="box--image" src={data.imgUrl} alt={data.title} />
               <h3 className="box--title">{data.title}</h3>
-            </div>
+            </motion.div>
           ))}
         </div>
     </section>

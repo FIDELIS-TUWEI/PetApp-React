4a80bbdf241f7374e39a083d5e29c45901433d05
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-03 11:51:33 +0300

    framer-motion configuration

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,30 @@
 import "../styles/banner.css";
-import groupImage from "../assets/pet-group.png"
+import groupImage from "../assets/pet-group.png";
+import { motion } from "framer-motion";
 
 const Banner = () => {
   return (
     <section className="banner--section">
       <div className="banner">
-        <div className="banner--content">
+        <motion.div 
+          className="banner--content"
+          initial={{ opacity: 0, scale: 0 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 2 }}
+        >
           <h2>Stay & Save</h2>
           <h2>Get your awesome discount</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
+        </motion.div>
 
         <div className="banner--image">
-          <img src={groupImage} alt="pet-group" />
+          <motion.img 
+            src={groupImage} 
+            alt="pet-group" 
+            initial={{ opacity: 0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 2 }}
+          />
         </div>
       </div>
     </section>

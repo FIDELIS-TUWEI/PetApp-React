8419defb09bdfcf12af6e2d99243ce4494c93317
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-03 11:51:45 +0300

    framer-motion configuration

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,17 @@
 import "../styles/home.css";
 import { HiOutlinePlay } from "react-icons/hi";
 import homeImage from "../assets/home-image.jpg";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <section className="home" id="home">
-      <div className="home--content">
+      <motion.div 
+        className="home--content"
+        initial={{ opacity: 0, x: -80 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 2 }}
+      >
         <h2 className="home--title">
           Bringing <span>Pets</span>
           <br />& People Closer
@@ -21,10 +27,15 @@ const Home = () => {
             <HiOutlinePlay className="btn--icon" /> Watch Video
           </button>
         </div>
-      </div>
-      <div className="home--image">
+      </motion.div>
+      <motion.div 
+        className="home--image"
+        initial={{ opacity: 0, scale: 0.5 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 2 }}
+      >
         <img src={homeImage} alt="Dog Lovers" />
-      </div>
+      </motion.div>
     </section>
   )
 }
